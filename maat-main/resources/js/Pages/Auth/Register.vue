3ddf4a7fcdcf2caac7736b840401973cc011871d
Erick Llanos <erick.llanos.clase@gmail.com>
2023-03-23 23:49:18 +0100

    ultima modificacion registro

--- maat-main/resources/js/Pages/Auth/Register.vue
+++ maat-main/resources/js/Pages/Auth/Register.vue
@@ -8,7 +8,6 @@ export default {
     registerButton.addEventListener("click", () => {
       container.classList.add("right-panel-active");
     });
-
     loginButton.addEventListener("click", () => {
       container.classList.remove("right-panel-active");
     });
