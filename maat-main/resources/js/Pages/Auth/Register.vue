cb6092a346f4bd2f2f7d3c0254827c33ab60662e
Erick Llanos <erick.llanos.clase@gmail.com>
2023-03-25 13:21:39 +0100

    Agregando Shadow Box al registro y login

--- maat-main/resources/js/Pages/Auth/Register.vue
+++ maat-main/resources/js/Pages/Auth/Register.vue
@@ -4,7 +4,6 @@ export default {
     const registerButton = document.getElementById("register");
     const loginButton = document.getElementById("login");
     const container = document.getElementById("container");
-
     registerButton.addEventListener("click", () => {
       container.classList.add("right-panel-active");
     });
@@ -12,6 +11,7 @@ export default {
       container.classList.remove("right-panel-active");
     });
   },
+  components: { GuestLayout }
 };
 </script>
 <script setup>
@@ -19,7 +19,9 @@ import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import TextInput from "@/Components/TextInput.vue";
+import GuestLayout from "@/Layouts/GuestLayout.vue";
 import { Head, Link, useForm } from "@inertiajs/vue3";
+import ShadowBox from "@/Components/ShadowBox.vue";
 
 const form = useForm({
   nombre_empresa: "",
@@ -40,80 +42,92 @@ const submit = () => {
   <Head title="Registro" />
   <div class="container" id="container">
     <div class="form-container register-container">
-      <form @submit.prevent="submit">
-        <div>
-          <InputLabel for="nombre_empresa" value="Nombre Empresa" class="block text-sm font-medium mb-1" />
-          <TextInput id="nombre_empresa" type="text" v-model="form.nombre_empresa" required autofocus />
-          <InputError class="mt-2" :message="form.errors.nombre_empresa" />
-        </div>
-        <div>
-          <InputLabel for="nif" value="NIF" />
-          <TextInput id="nif" type="text" v-model="form.nif" required autofocus />
-        </div>
-        <div>
-          <InputLabel for="correo" value="Correo" />
-          <TextInput id="correo" type="email" v-model="form.correo" required autofocus />
-          <InputError class="mt-2" :message="form.errors.correo" />
-        </div>
-        <div>
-          <InputLabel for="password" value="Contraseña" />
-          <TextInput id="password" type="password" v-model="form.password" required autocomplete="new-password" />
-          <InputError class="mt-2" :message="form.errors.password" />
-        </div>
-        <div class="mt-4">
-          <InputLabel for="password_confirmation" value="Confirma la contraseña" />
-          <TextInput id="password_confirmation" type="password" v-model="form.password_confirmation" required
-            autocomplete="new-password" />
-          <InputError class="mt-2" :message="form.errors.password_confirmation" />
-        </div>
-        <div>
-          <PrimaryButton class="boton" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-            Registrar
-          </PrimaryButton>
-          <hr />
-        </div>
-        <Link :href="route('login')" class="underline">
-        ¿Ya está registrado?
-        </Link>
-      </form>
+      <ShadowBox>
+        <form @submit.prevent="submit">
+          <div>
+            <InputLabel for="nombre_empresa" value="Nombre Empresa" class="block text-sm font-medium mb-1" />
+            <TextInput id="nombre_empresa" type="text" v-model="form.nombre_empresa" required autofocus
+              class="form-input w-full" />
+            <InputError class="mt-2" :message="form.errors.nombre_empresa" />
+          </div>
+          <div class="mt-4">
+            <InputLabel for="nif" value="NIF" class="block text-sm font-medium mb-1" />
+            <TextInput id="nif" type="text" v-model="form.nif" required autofocus class="form-input w-full" />
+          </div>
+          <div class="mt-4">
+            <InputLabel for="correo" value="Correo" class="block text-sm font-medium mb-1" />
+            <TextInput id="correo" type="email" v-model="form.correo" required autofocus class="form-input w-full" />
+            <InputError class="mt-2" :message="form.errors.correo" />
+          </div>
+          <div class="mt-4">
+            <InputLabel for="password" value="Contraseña" class="block text-sm font-medium mb-1" />
+            <TextInput id="password" type="password" v-model="form.password" required autocomplete="new-password"
+              class="form-input w-full" />
+            <InputError class="mt-3" :message="form.errors.password" />
+          </div>
+          <div class="mt-4">
+            <InputLabel for="password_confirmation" value="Confirma la contraseña"
+              class="block text-sm font-medium mb-1" />
+            <TextInput id="password_confirmation" type="password" v-model="form.password_confirmation" required
+              autocomplete="new-password" class="form-input w-full" />
+            <InputError class="mt-3 form-input w-full" :message="form.errors.password_confirmation" />
+          </div>
+          <div class="mt-3">
+            <PrimaryButton class="boton mt-3 ml-0" :class="{ 'opacity-25': form.processing }"
+              :disabled="form.processing">
+              Registrar
+            </PrimaryButton>
+            <hr />
+          </div>
+          <Link :href="route('login')" class="underline mt-3">
+          ¿Ya está registrado?
+          </Link>
+        </form>
+      </ShadowBox>
     </div>
     <div class="form-container login-container">
-      <form @submit.prevent="submit">
-        <div>
-          <InputLabel for="nombre_empresa" value="Nombre ONG" />
-          <TextInput id="nombre_empresa" type="text" v-model="form.nombre_empresa" required autofocus />
-          <InputError class="mt-2" :message="form.errors.nombre_empresa" />
-        </div>
-        <div>
-          <InputLabel for="nif" value="NIF" />
-          <TextInput id="nif" type="text" v-model="form.nif" required autofocus />
-        </div>
-        <div>
-          <InputLabel for="correo" value="Correo" />
-          <TextInput id="correo" type="email" v-model="form.correo" required autofocus />
-          <InputError class="mt-2" :message="form.errors.correo" />
-        </div>
-        <div>
-          <InputLabel for="password" value="Contraseña" />
-          <TextInput id="password" type="password" v-model="form.password" required autocomplete="new-password" />
-          <InputError class="mt-2" :message="form.errors.password" />
-        </div>
-        <div class="mt-4">
-          <InputLabel for="password_confirmation" value="Confirma la contraseña" />
-          <TextInput id="password_confirmation" type="password" v-model="form.password_confirmation" required
-            autocomplete="new-password" />
-          <InputError class="mt-2" :message="form.errors.password_confirmation" />
-        </div>
-        <div>
-          <PrimaryButton class="boton" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-            Registrar
-          </PrimaryButton>
-          <hr />
-        </div>
-        <Link :href="route('login')" class="underline">
-        ¿Ya está registrado?
-        </Link>
-      </form>
+      <ShadowBox>
+        <form @submit.prevent="submit" class="space-y-4">
+          <div>
+            <InputLabel for="nombre_empresa" value="Nombre ONG" class="block text-sm font-medium mb-1" />
+            <TextInput id="nombre_empresa" type="text" v-model="form.nombre_empresa" required autofocus
+              class="form-input w-full" />
+            <InputError class="mt-2" :message="form.errors.nombre_empresa" />
+          </div>
+          <div class="mt-4">
+            <InputLabel for="nif" value="NIF" class="block text-sm font-medium mb-1" />
+            <TextInput id="nif" type="text" v-model="form.nif" required autofocus class="form-input w-full" />
+          </div>
+          <div class="mt-4">
+            <InputLabel for="correo" value="Correo" class="block text-sm font-medium mb-1" />
+            <TextInput id="correo" type="email" v-model="form.correo" required autofocus class="form-input w-full" />
+            <InputError class="mt-2" :message="form.errors.correo" />
+          </div>
+          <div class="mt-4">
+            <InputLabel for="password" value="Contraseña" class="block text-sm font-medium mb-1" />
+            <TextInput id="password" type="password" v-model="form.password" required autocomplete="new-password"
+              class="form-input w-full" />
+            <InputError class="mt-2" :message="form.errors.password" />
+          </div>
+          <div class="mt-4">
+            <InputLabel for="password_confirmation" value="Confirma la contraseña"
+              class="block text-sm font-medium mb-1" />
+            <TextInput id="password_confirmation" type="password" v-model="form.password_confirmation" required
+              autocomplete="new-password" class="form-input w-full" />
+            <InputError class="mt-2" :message="form.errors.password_confirmation" />
+          </div>
+          <div>
+            <PrimaryButton class="boton btn text-white ml-0" :class="{ 'opacity-25': form.processing }"
+              :disabled="form.processing">
+              Registrar
+            </PrimaryButton>
+            <hr />
+          </div>
+          <Link :href="route('login')" class="underline mt-3">
+          ¿Ya está registrado?
+          </Link>
+        </form>
+      </ShadowBox>
     </div>
     <div class="overlay-container">
       <div class="overlay">
@@ -130,32 +144,6 @@ const submit = () => {
   </div>
 </template>
 <style>
-.content {
-  display: flex;
-  width: 100%;
-  height: 50px;
-  align-items: center;
-  justify-content: space-around;
-}
-
-.content .checkbox {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.content input {
-  accent-color: #333;
-  width: 12px;
-  height: 12px;
-}
-
-.content label {
-  font-size: 14px;
-  user-select: none;
-  padding-left: 5px;
-}
-
 button {
   position: relative;
   border-radius: 50px;
@@ -175,6 +163,7 @@ button {
   letter-spacing: 3px;
   background-color: #73cd99;
 } */
+
 button:hover {
   letter-spacing: 3px;
 }
@@ -188,9 +177,9 @@ button:hover {
 }
 
 button.ghost {
-  background-color: rgba(225, 225, 225, 0.2);
+  background-color: #73cd99;
   border: 2px solid #fff;
-  color: #fff;
+  color: #333;
 }
 
 button.ghost i {
@@ -199,25 +188,7 @@ button.ghost i {
   transition: 0.3s ease-in-out;
 }
 
-button.ghost i.register {
-  right: 70px;
-}
-
-button.ghost i.login {
-  left: 70px;
-}
-
-button.ghost:hover i.register {
-  right: 40px;
-  opacity: 1;
-}
-
-button.ghost:hover i.login {
-  left: 40px;
-  opacity: 1;
-}
-
-form {
+/* form {
   background-color: #fff;
   display: flex;
   align-items: center;
@@ -225,7 +196,7 @@ form {
   flex-direction: column;
   height: 100%;
   text-align: left;
-}
+} */
 
 .form-container {
   position: absolute;
@@ -310,11 +281,9 @@ form {
   right: 0;
   top: 0;
   bottom: 0;
-  /* background: linear-gradient(
-    to top,
-    rgba(46, 94, 109, 0.4) 40%,
-    rgba(46, 94, 109, 0)
-  ); */
+  background: linear-gradient(to top,
+      rgba(46, 94, 109, 0.4) 40%,
+      rgba(46, 94, 109, 0));
 }
 
 .container.right-panel-active .overlay {
@@ -351,25 +320,4 @@ form {
 
 .container.right-panel-active .overlay-right {
   transform: translateX(20%);
-}
-
-.social-container {
-  margin: 20px 0;
-}
-
-.social-container a {
-  border: 1px solid #dddddd;
-  border-radius: 50%;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0 5px;
-  height: 40px;
-  width: 40px;
-  transition: 0.3s ease-in-out;
-}
-
-.social-container a:hover {
-  border: 1px solid #4bb6b7;
-}
-</style>
+}</style>
