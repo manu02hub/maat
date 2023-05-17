a807359351061614058527ab0e797b968326bed0
mariognzs <mariogonzalezsolas@gmail.com>
2023-05-17 22:20:57 +0200

    cambio post

--- maat-main/resources/js/Components/manuComponents/EventInfoComponent.vue
+++ maat-main/resources/js/Components/manuComponents/EventInfoComponent.vue
@@ -221,23 +221,6 @@ const form2 = useForm({
                                     </div>
                                 </button>
                                 </Link>
-
-                                <!-- Share button -->
-                                <button class="flex items-center text-slate-400 hover:text-emerald-500">
-                                    <svg class="w-4 h-4 shrink-0 fill-current mr-1.5" viewBox="0 0 16 16">
-                                        <path
-                                            d="M13 7h2v6a1 1 0 0 1-1 1H4v2l-4-3 4-3v2h9V7ZM3 9H1V3a1 1 0 0 1 1-1h10V0l4 3-4 3V4H3v5Z" />
-                                    </svg>
-                                    <div class="text-sm text-slate-500">{{ }}</div>
-                                </button>
-                                <!-- Replies button -->
-                                <button class="flex items-center text-slate-400 hover:text-amber-500">
-                                    <svg class="w-4 h-4 shrink-0 fill-current mr-1.5" viewBox="0 0 16 16">
-                                        <path
-                                            d="M8 0C3.6 0 0 3.1 0 7s3.6 7 8 7h.6l5.4 2v-4.4c1.2-1.2 2-2.8 2-4.6 0-3.9-3.6-7-8-7zm4 10.8v2.3L8.9 12H8c-3.3 0-6-2.2-6-5s2.7-5 6-5 6 2.2 6 5c0 2.2-2 3.8-2 3.8z" />
-                                    </svg>
-                                    <div class="text-sm text-slate-500"> {{ }}</div>
-                                </button>
                             </footer>
                         </div>
                     </header>
@@ -308,22 +291,6 @@ const form2 = useForm({
                                             </div>
                                             <!-- Footer -->
                                             <footer class="flex items-center space-x-4">
-                                                <!-- Like button -->
-                                                <button class="flex items-center text-slate-400 hover:text-red-600">
-                                                    <svg class="w-4 h-4 shrink-0 fill-current mr-1.5" viewBox="0 0 16 16">
-                                                        <path
-                                                            d="M14.682 2.318A4.485 4.485 0 0011.5 1 4.377 4.377 0 008 2.707 4.383 4.383 0 004.5 1a4.5 4.5 0 00-3.182 7.682L8 15l6.682-6.318a4.5 4.5 0 000-6.364zm-1.4 4.933L8 12.247l-5.285-5A2.5 2.5 0 014.5 3c1.437 0 2.312.681 3.5 2.625C9.187 3.681 10.062 3 11.5 3a2.5 2.5 0 011.785 4.251h-.003z" />
-                                                    </svg>
-                                                    <div class="text-sm text-slate-500">{{ comentarios.like }}</div>
-                                                </button>
-                                                <!-- Share button -->
-                                                <button class="flex items-center text-slate-400 hover:text-emerald-500">
-                                                    <svg class="w-4 h-4 shrink-0 fill-current mr-1.5" viewBox="0 0 16 16">
-                                                        <path
-                                                            d="M13 7h2v6a1 1 0 0 1-1 1H4v2l-4-3 4-3v2h9V7ZM3 9H1V3a1 1 0 0 1 1-1h10V0l4 3-4 3V4H3v5Z" />
-                                                    </svg>
-                                                    <div class="text-sm text-slate-500">{{ comentarios.repost }}</div>
-                                                </button>
                                                 <Link :href="route('deleteComentario', comentarios.id)">
                                                 <button class="action-buttons-list" @click="Eliminar">
                                                     <svg class="h-6 w-6 text-red-500" viewBox="0 0 24 24" stroke-width="2"
